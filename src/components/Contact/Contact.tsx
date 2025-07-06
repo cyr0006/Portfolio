c7a6a04fba894d5a92e96dc4ba442f2db37c7b16
Aryan <Aryan.m10@yahoo.com>
2025-07-06 12:42:16 +1000

    format changes to contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -60,8 +60,8 @@ export const Contact = () => {
             Aryan.m10@yahoo.com{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
+              width="16"
+              height="16"
               fill="currentColor"
               className="bi bi-envelope"
               viewBox="0 0 16 16"
