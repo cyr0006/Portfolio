97201188e18f8adeca701b887b9c5664d46b26e1
Aryan <Aryan.m10@yahoo.com>
2025-10-29 20:15:21 +1100

    components/Frameworks arrow button animation and positioning; now buttons with indi css, no svg focus

--- src/components/Frameworks/Frameworks.tsx
+++ src/components/Frameworks/Frameworks.tsx
@@ -1,4 +1,6 @@
 import "../Frameworks/Frameworks.css";
+import { motion, scale } from "framer-motion";
+
 import react_logo from "../../assets/react-logo.png";
 import mongodb_logo from "../../assets/mongodb_logo.png";
 import nodejs_logo from "../../assets/nodejs_logo.png";
@@ -26,34 +28,52 @@ const Frameworks = () => {
 
   return (
     <div className="Frameworks">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="60"
-        height="60"
-        fill="wheat"
-        className="bi bi-arrow-right-circle right"
-        viewBox="0 0 16 16"
-        onClick={slideFwd}
-      >
-        <path
-          fill-rule="evenodd"
-          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="60"
-        height="60"
-        fill="wheat"
-        className="bi bi-arrow-left-circle"
-        viewBox="0 0 16 16"
+      <button onClick={slideFwd} className="x-button right">
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="wheat"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-right-circle"
+          whileHover={{ scale: 1.2, opacity: 0.8 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          <path
+            fillRule="evenodd"
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+          />
+        </motion.svg>
+      </button>
+      <button
         onClick={slideBwd}
+        className="x-button left"
+        style={{
+          background: "none",
+          border: "none",
+          padding: 0,
+          margin: 0,
+          cursor: "pointer",
+        }}
       >
-        <path
-          fill-rule="evenodd"
-          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
-        />
-      </svg>
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="wheat"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-left-circle"
+          whileHover={{ scale: 1.2, opacity: 0.8 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          <path
+            fillRule="evenodd"
+            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+          />
+        </motion.svg>
+      </button>
       <div className="slider">
         <ul ref={slider}>
           <li>
