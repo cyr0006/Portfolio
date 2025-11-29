e1ec6f4db44eb57b034ab902d984129dc15fbbad
Aryan <Aryan.m10@yahoo.com>
2025-11-29 21:19:50 +1100

    scoped tailwind directives

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import ChatComponent from "../components/Chat/ChatComponent";
-import "tailwindcss/tailwind.css";
+import "../../src/chat.css";
 
 const Chat: React.FunctionComponent = () => {
   return (
-    <div className="max-w-lg mt-20 mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-      <div className="w-full max-w-lg bg-white shadow-lg rounded-lg overflow-hidden">
-        <ChatComponent />
+    <div className="chatbot-root">
+      <div className="max-w-lg mt-20 mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="w-full max-w-lg bg-white shadow-lg rounded-lg overflow-hidden">
+          <ChatComponent />
+        </div>
       </div>
     </div>
   );
