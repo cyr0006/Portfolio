e4780de4bc5524c7e6409a829d1b73ec51207a4e
Aryan <Aryan.m10@yahoo.com>
2025-11-30 19:14:09 +1100

    rolled back page-container to container in index css

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,11 +3,9 @@ import ChatComponent from "../components/Chat/ChatComponent";
 
 const Chat: React.FunctionComponent = () => {
   return (
-    <div className="chatbot-container">
-      <div className="max-w-lg mt-20 mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-        <div className="w-full max-w-lg bg-white shadow-lg rounded-lg overflow-hidden">
-          <ChatComponent />
-        </div>
+    <div className="max-w-lg mt-20 mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+      <div className="w-full max-w-lg bg-white shadow-lg rounded-lg overflow-hidden">
+        <ChatComponent />
       </div>
     </div>
   );
