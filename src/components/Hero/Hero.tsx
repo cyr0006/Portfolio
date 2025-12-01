10eed74730aebbba282713d280e0c357014a7d01
Aryan <Aryan.m10@yahoo.com>
2025-12-01 21:07:42 +1100

    Made AI button go to AI

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import "./Hero.css";
 import r_arrow from "../../assets/r-arrow.png";
 import { motion, scale } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -12,15 +13,17 @@ const Hero = () => {
           particular bit of information is deeply inflationary.
         </p>
         <p>Click the button below to ask my AI Instead.</p>
-        <motion.button
-          type="submit"
-          className="btn light-btn"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          Explore More
-          <img src={r_arrow} alt="" />
-        </motion.button>
+        <Link to="/chat">
+          <motion.button
+            type="submit"
+            className="btn light-btn"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Get Started
+            <img src={r_arrow} alt="" />
+          </motion.button>
+        </Link>
       </div>
     </div>
   );
